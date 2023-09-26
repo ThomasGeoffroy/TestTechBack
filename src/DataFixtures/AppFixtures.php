29550074ade459b165969bb58119ef9d6b1c6ec8
Etudiant O'clock <thomasgeoffroy90@gmail.com>
2023-09-26 15:33:22 +0200

    Fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Article;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+
+            // Creation of a french locale for the faker
+            $faker = Faker\Factory::create("fr_FR");
+    
+            // Use of a populator
+            $populator = new \Faker\ORM\Doctrine\Populator($faker, $manager);
+    
+    
+            // creation of the category class to populate
+            $populator->addEntity(Article::class, 15,
+            [
+                "title" => function () use ($faker) {
+                    return $faker->text(15);
+                },
+                "status" => function () use ($faker) {
+                    return $faker->numberBetween(0,1);
+                },
+                "text" => function () use ($faker) {
+                    return $faker->text(200);
+                },
+                "author" => function () use ($faker) {
+                    return $faker->name();
+                }
+            ]
+            );
+    
+            // We execute the populator to add the data locally
+            $insertedItems = $populator->execute();
+
+        $manager->flush();
+    }
+}
